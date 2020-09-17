abd414ef47368f53cd3e12a8b3b4ab614af2551b
Eduardo Regis <eduregis1@gmail.com>
2020-09-16 22:10:00 -0300

    creating a secondary table view to picker view

--- ComicApp/Controller/AddToShelfViewController.swift
+++ ComicApp/Controller/AddToShelfViewController.swift
@@ -9,27 +9,244 @@
 import UIKit
 
 class AddToShelfViewController: UITableViewController {
-
+    
+    var comicTitle: String?
+    var imageURL: String?
+    var progressNumber: Int?
+    var finishNumber: Int?
+    var type: String?
+    var organizeBy: String?
+    var status: String?
+    var author: String?
+    var artist: String?
+    
+    let comicTitleTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+    let stepper = UIStepper()
+    let finishNumberTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+    let typeTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+    let authorTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    let artistTextField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+    
+    let pickerView = UIPickerView()
+    
+    let typePickerViewData = ["Quadrinho", "Livro"]
+    let organizeByData = ["Página", "Capítulo", "Volume"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Criar Item"
         navigationItem.largeTitleDisplayMode = .never
         tableView.tableFooterView = UIView()
-        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+        initNewitem()
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
-   
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func initNewitem() {
+        comicTitle = "-"
+        progressNumber = 0
+        organizeBy = "Volume"
+    }
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        comicTitleTextField.resignFirstResponder()
+        finishNumberTextField.resignFirstResponder()
+        typeTextField.resignFirstResponder()
+        authorTextField.resignFirstResponder()
+        artistTextField.resignFirstResponder()
+    }
+    
     @IBAction func doneButton(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 9
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "setInfo", for: indexPath)
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Nome "
+            comicTitleTextField.textAlignment = .right
+            comicTitleTextField.placeholder = "-"
+            cell.accessoryView = comicTitleTextField
+        case 1:
+            if let organizeBy = organizeBy {
+                if let progressNumber = progressNumber {
+                    stepper.value = Double(progressNumber)
+                    stepper.addTarget(self, action: #selector(stepperValueChanged), for: .valueChanged)
+                    if let finishNumber = finishNumber {
+                        cell.textLabel?.text = "\(progressNumber)/\(finishNumber) (em \(organizeBy.lowercased())s)"
+                    } else {
+                        cell.textLabel?.text = "\(progressNumber)/- (em \(organizeBy.lowercased())s)"
+                    }
+                }
+            }
+            cell.accessoryView = stepper
+        case 2:
+            cell.textLabel?.text = "\(organizeBy ?? "-") final "
+            finishNumberTextField.textAlignment = .right
+            finishNumberTextField.placeholder = "-"
+            cell.accessoryView = finishNumberTextField
+        case 3:
+            cell.textLabel?.text = "Tipo "
+            typeTextField.textAlignment = .right
+            typeTextField.placeholder = "Quadrinho"
+            let toolbar = UIToolbar()
+            toolbar.sizeToFit()
+            let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(changeType))
+            toolbar.setItems([doneBtn], animated: true)
+            typeTextField.inputAccessoryView = toolbar
+            typeTextField.inputView = pickerView
+            cell.accessoryView = typeTextField
+        case 4:
+            cell.textLabel?.text = "Organizar por "
+            let button = UIButton(type: .custom)
+            button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+            button.setTitle("Volume ", for: .normal)
+            button.setTitleColor(.systemGray, for: .normal)
+            button.addTarget(self, action: #selector(changeOrganizeBy), for: .touchUpInside)
+            button.tag = indexPath.row
+            button.semanticContentAttribute = .forceRightToLeft
+            button.sizeToFit()
+            cell.accessoryView = button
+        case 5:
+            cell.textLabel?.text = "Status "
+            let button = UIButton(type: .custom)
+            button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+            button.setTitle("Quero Ler ", for: .normal)
+            button.setTitleColor(.systemGray, for: .normal)
+            button.addTarget(self, action: #selector(changeStatus), for: .touchUpInside)
+            button.tag = indexPath.row
+            button.semanticContentAttribute = .forceRightToLeft
+            button.sizeToFit()
+            cell.accessoryView = button
+        case 6:
+            cell.textLabel?.text = "Autor/Roteiro "
+            authorTextField.textAlignment = .right
+            authorTextField.placeholder = "-"
+            cell.accessoryView = authorTextField
+        case 7:
+            cell.textLabel?.text = "Ilustração "
+            artistTextField.textAlignment = .right
+            artistTextField.placeholder = "-"
+            cell.accessoryView = artistTextField
+        default:
+            break
+        }
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16.0)
+        cell.backgroundColor = .systemGray6
+        return cell
+    }
+    
+    @objc func stepperValueChanged() {
+        progressNumber = Int(stepper.value)
+        tableView.reloadData()
+    }
+    
+    @objc func changeType() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func changeOrganizeBy() {
+        performSegue(withIdentifier: "PickerItemViewSegue", sender: self)
+    }
+    
+    @objc func changeStatus() {
+        print("aaaa")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is PickerItemViewController {
+            let viewController = segue.destination as? PickerItemViewController
+            viewController?.pickerData = organizeByData
+        }
+    }
+    /*
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
+extension AddToShelfViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typePickerViewData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return typePickerViewData[row]
+    }
+    
+    
 }
